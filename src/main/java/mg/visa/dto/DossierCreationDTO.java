10d839973b7c5f8ac61a23c032fd62e9c64a87ee
Ambinintsoa01 <rakotoarisedyambinintsoa@gmail.com>
2026-04-26 20:42:36 +0300

    Update DTO dossier

--- src/main/java/mg/visa/dto/DossierCreationDTO.java
+++ src/main/java/mg/visa/dto/DossierCreationDTO.java
@@ -1,8 +1,12 @@
 package mg.visa.dto;
 
+import java.util.List;
+
 public class DossierCreationDTO {
     private Long demandeId;
     private String createdBy;
+    private List<Long> selectedCommuneIds;
+    private List<Long> selectedComplementaireIds;
 
     public DossierCreationDTO() {}
 
@@ -11,4 +15,10 @@ public class DossierCreationDTO {
 
     public String getCreatedBy() { return createdBy; }
     public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
+
+    public List<Long> getSelectedCommuneIds() { return selectedCommuneIds; }
+    public void setSelectedCommuneIds(List<Long> selectedCommuneIds) { this.selectedCommuneIds = selectedCommuneIds; }
+
+    public List<Long> getSelectedComplementaireIds() { return selectedComplementaireIds; }
+    public void setSelectedComplementaireIds(List<Long> selectedComplementaireIds) { this.selectedComplementaireIds = selectedComplementaireIds; }
 }
