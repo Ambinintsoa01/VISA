822be3aca8b2d9a9eb1c7e8bcc1fe3f42ef69cd8
Ambinintsoa01 <rakotoarisedyambinintsoa@gmail.com>
2026-04-26 20:41:32 +0300

    TypeDemandeRepo delete

--- src/main/java/mg/visa/repository/ref/TypeDemandeRepository.java
+++ src/main/java/mg/visa/repository/ref/TypeDemandeRepository.java
@@ -1,8 +1,9 @@
-package mg.visa.repository;
+package mg.visa.repository.ref;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mg.visa.entity.ref.TypeDemande;
 
 public interface TypeDemandeRepository extends JpaRepository<TypeDemande, Long> {
+    java.util.Optional<TypeDemande> findByCode(String code);
 }
