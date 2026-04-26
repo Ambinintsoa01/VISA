6f0e41ff7faa228e498a75b2fe9cc85df0025987
Ambinintsoa01 <rakotoarisedyambinintsoa@gmail.com>
2026-04-26 20:44:52 +0300

    Add getters to RefDataService

--- src/main/java/mg/visa/service/RefDataService.java
+++ src/main/java/mg/visa/service/RefDataService.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Service;
 
 import mg.visa.entity.ref.Nationalite;
 import mg.visa.entity.ref.SituationFamiliale;
+import mg.visa.entity.ref.TypeDemande;
 import mg.visa.entity.ref.TypeIdentite;
+import mg.visa.entity.ref.TypeVisa;
 import mg.visa.repository.ref.NationaliteRepository;
 import mg.visa.repository.ref.SituationFamilialeRepository;
+import mg.visa.repository.ref.TypeDemandeRepository;
 import mg.visa.repository.ref.TypeIdentiteRepository;
+import mg.visa.repository.ref.TypeVisaRepository;
 
 @Service
 public class RefDataService {
@@ -17,13 +21,19 @@ public class RefDataService {
     private final NationaliteRepository nationaliteRepository;
     private final SituationFamilialeRepository situationFamilialeRepository;
     private final TypeIdentiteRepository typeIdentiteRepository;
+    private final TypeVisaRepository typeVisaRepository;
+    private final TypeDemandeRepository typeDemandeRepository;
 
     public RefDataService(NationaliteRepository nationaliteRepository,
                           SituationFamilialeRepository situationFamilialeRepository,
-                          TypeIdentiteRepository typeIdentiteRepository) {
+                          TypeIdentiteRepository typeIdentiteRepository,
+                          TypeVisaRepository typeVisaRepository,
+                          TypeDemandeRepository typeDemandeRepository) {
         this.nationaliteRepository = nationaliteRepository;
         this.situationFamilialeRepository = situationFamilialeRepository;
         this.typeIdentiteRepository = typeIdentiteRepository;
+        this.typeVisaRepository = typeVisaRepository;
+        this.typeDemandeRepository = typeDemandeRepository;
     }
 
     public List<Nationalite> getNationalites() {
@@ -36,5 +46,13 @@ public class RefDataService {
 
     public List<TypeIdentite> getTypesIdentite() {
         return typeIdentiteRepository.findAll();
+    }
+
+    public List<TypeVisa> getTypesVisas() {
+        return typeVisaRepository.findAll();
+    }
+
+    public List<TypeDemande> getTypesDemandes() {
+        return typeDemandeRepository.findAll();
     }
 }
